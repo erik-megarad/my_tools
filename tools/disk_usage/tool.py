d5c6fcd54e6bfe9020358fd84cea3bd1ccf8b9c8
Erik Peterson <e@eriklp.com>
2023-06-28 17:59:40 -0700

    Allow directory to be passed to disk_usage

--- tools/disk_usage/tool.py
+++ tools/disk_usage/tool.py
@@ -1,6 +1,6 @@
 import json
-import psutil
 
+import psutil
 from langchain.tools import BaseTool
 
 
@@ -8,18 +8,23 @@ class DiskUsageTool(BaseTool):
     """
     An example tool without any base classes that implements the bare minimum needed to adhere to the interface.
     """
+
     name = "disk_usage"
-    description = "Useful for when you want to know the disk usage on the current system"
+    description = (
+        "Useful for when you want to know the disk usage on the current system"
+    )
 
-    def _run(self, *args, **kwargs) -> str:
-        disk_usage = psutil.disk_usage('/')
+    def _run(self, directory: str = "/") -> str:
+        disk_usage = psutil.disk_usage(directory)
 
-        return json.dumps({
-            "total": disk_usage.total,
-            "used": disk_usage.used,
-            "free": disk_usage.free,
-            "percent": disk_usage.percent
-        })
+        return json.dumps(
+            {
+                "total": disk_usage.total,
+                "used": disk_usage.used,
+                "free": disk_usage.free,
+                "percent": disk_usage.percent,
+            }
+        )
 
-    async def _arun(self, *args, **kwargs) -> str:
-        return self._run(*args, **kwargs)
+    async def _arun(self, directory: str = "/") -> str:
+        return self._run()
--- tools/os_info/tool.py
+++ tools/os_info/tool.py
@@ -8,11 +8,14 @@ class OSInfo(BaseTool):
     """
     Example tool using the LangChain tool base class
     """
+
     name = "os_info"
     description = "Get the current OS name and version information"
 
     def _run(self) -> str:
-        return json.dumps({"os_name": platform.system(), "os_version": platform.release()})
+        return json.dumps(
+            {"os_name": platform.system(), "os_version": platform.release()}
+        )
 
     async def _arun(self) -> str:
         return self._run()
